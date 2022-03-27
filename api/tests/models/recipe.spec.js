07ffdbebbf7e82482324511ae5dde247095ccd98
CamiloTECH <camilomontoya10@live.com>
2022-03-27 11:36:58 -0500

    Fin testing Modelos

--- api/tests/models/recipe.spec.js
+++ api/tests/models/recipe.spec.js
@@ -12,7 +12,6 @@ describe("Models", () => {
     beforeEach(async () => await Recipe.sync({ force: true }));
 
     describe("Title", () => {
-
       it("should throw an error if title is null", (done) => {
         Recipe.create({
           summary: "summary",
@@ -24,7 +23,6 @@ describe("Models", () => {
     });
 
     describe("Summary", () => {
-
       it("should throw an error if summary is null", (done) => {
         Recipe.create({
           title: "Title",
@@ -36,7 +34,6 @@ describe("Models", () => {
     });
 
     describe("Image", () => {
-
       it("should throw an error if image is null", (done) => {
         Recipe.create({
           title: "Title",
@@ -48,14 +45,13 @@ describe("Models", () => {
     });
 
     describe("Enter data", () => {
-
       it("should throw an error if all data is null", (done) => {
         Recipe.create({})
           .then(() => done(new Error("The information cannot be null")))
           .catch(() => done());
       });
 
-      it("should work when its a valid name", (done) => {
+      it("should work when its a valid data", (done) => {
         Recipe.create({
           title: "Milanesa a la napolitana",
           summary: "summary",
@@ -66,10 +62,50 @@ describe("Models", () => {
       });
     });
 
+    describe("Score and HealthScore", () => {
+      it("should throw an error if data type of score and healthScore is string", (done) => {
+        Recipe.create({
+          title:"Title",
+          summary:"summary",
+          image:"image",
+          score:"string",
+          healthScore:"string"
+        })
+          .then(() => done(new Error("The score property cannot be a string")))
+          .catch(() => done());
+      });
+    });
   });
 
-  describe("Diet Model",()=>{
-    
-  })
+  describe("Diet Model", () => {
+    beforeEach(async () => await Diet.sync({ force: true }));
 
+    describe("Name", () => {
+      it("should throw an error if name is null", (done) => {
+        Diet.create({})
+          .then(() => done(new Error("The name property cannot be null")))
+          .catch(() => done());
+      });
+
+      it("should work when its a valid name", (done) => {
+        Diet.create({
+          name: "gluten free",
+        })
+          .then(() => done())
+          .catch((err) => done(err));
+      });
+
+      it("should throw an error if there are two equal names", (done) => {
+        Diet.create({
+          name: "gluten free",
+        })
+
+        Diet.create({
+          name: "gluten free",
+        })
+          .then(() => done(new Error("There cannot be two names equals")))
+          .catch(() => done());
+      });
+    });
+  });
 });
