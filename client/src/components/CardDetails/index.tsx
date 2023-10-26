a22c984741b4ac842d931d52ebe3e350bb9bceef
Camilo Montoya <camilo.montoya@masterbase.com>
2023-10-25 21:04:57 -0500

    improve design detail

--- client/src/components/CardDetails/index.tsx
+++ client/src/components/CardDetails/index.tsx
@@ -1,3 +1,5 @@
+import DOMPurify from "dompurify";
+import { Parser } from "html-to-react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
@@ -10,6 +12,8 @@ import {
 } from "../../redux/actions";
 import Loading from "../Loading";
 import style from "./CardDetails.module.css";
+
+const parser = Parser();
 
 function CardDetail() {
   const { id } = useParams();
@@ -47,47 +51,76 @@ function CardDetail() {
           <h1>Recipe with ID {id} not found </h1>
         ) : (
           <div className={style.card}>
-            <div>
-              <div>
-                <h1>{recipesDetail.title}</h1>
-                <div>
-                  <p
-                    dangerouslySetInnerHTML={{ __html: recipesDetail.summary }}
-                  />
+            <div className={style.headerInfo}>
+              <div className={style.contentGeneraInfo}>
+                <h1 className={style.title}>{recipesDetail.title}</h1>
+                <div className={style.dataHealth}>
+                  <div className={style.scores}>
+                    <span className={style.scoresTitle}>Score:</span>{" "}
+                    {recipesDetail.score}⭐
+                  </div>
+                  <div className={style.scores}>
+                    <span className={style.scoresTitle}>HealtScore:</span>{" "}
+                    {recipesDetail.healthScore}🌿
+                  </div>
                 </div>
-                <p>
-                  <b>Score:</b> {recipesDetail.score}⭐
-                </p>
-                <p>
-                  <b>HealtScore:</b> {recipesDetail.healthScore}🌿
-                </p>
               </div>
-              <img src={recipesDetail.image} alt="Not found" />
+
+              <div className={style.contentImage}>
+                <img
+                  className={style.image}
+                  src={recipesDetail.image}
+                  alt="Not found"
+                />
+              </div>
             </div>
-            <div>
-              <div>
-                <div>
-                  <p>Type of Diet:</p>
-                  <ul>
+
+            <div className={style.contentAllInfo}>
+              <div className={style.contentTypes}>
+                <div className={style.typesDiet}>
+                  <span className={style.titleType}>Type of Diet</span>
+                  <ul className={style.listTypes}>
                     {recipesDetail.diets.map((diet, index) => (
-                      <li key={index}>{diet}</li>
+                      <li key={index} className={style.listItem}>
+                        {diet}
+                      </li>
                     ))}
                   </ul>
                 </div>
                 {recipesDetail.dishes && (
-                  <div>
-                    <p>Type of Dish:</p>
-                    <ul>
+                  <div className={style.typesDish}>
+                    <span className={style.titleType}>Type of Dish</span>
+                    <ul className={style.listTypes}>
                       {recipesDetail.dishes.map((dish, index) => (
-                        <li key={index}>{dish}</li>
+                        <li key={index} className={style.listItem}>
+                          {dish}
+                        </li>
                       ))}
                     </ul>
                   </div>
                 )}
               </div>
-              <div>
-                <h2>Steps:</h2>
-                <p dangerouslySetInnerHTML={{ __html: recipesDetail.steps }} />
+              <div className={style.contentMainInfo}>
+                <div className={style.summary}>
+                  <h2 className={style.titleSummary}>Summary:</h2>
+                  <div className={style.contentSummary}>
+                    {parser.parse(
+                      DOMPurify.sanitize(recipesDetail.summary, {
+                        ADD_ATTR: ["target"]
+                      })
+                    )}
+                  </div>
+                </div>
+                <div className={style.steps}>
+                  <h2 className={style.titleSteps}>Steps:</h2>
+                  <div className={style.contentSteps}>
+                    {parser.parse(
+                      DOMPurify.sanitize(recipesDetail.steps, {
+                        ADD_ATTR: ["target"]
+                      })
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
