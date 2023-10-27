bd4364bc9a22b7c0208b4538b1604aed444b3825
Camilo Montoya <camilo.montoya@masterbase.com>
2023-10-26 21:03:13 -0500

    improve design form

--- client/src/components/CreateRecipe/index.tsx
+++ client/src/components/CreateRecipe/index.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { ReducerState } from "../../models";
 import {
@@ -98,218 +98,221 @@ function CreateRecipe() {
 
   return (
     <form onSubmit={handleSubmit} className={style.form}>
-      <h2>Create a new recipe</h2>
+      <h2 className={style.titleForm}>Create a new recipe</h2>
+
       <div className={style.data}>
-        <div className={style.message}>
-          <label htmlFor="title" className={style.name}>
-            Name
-          </label>
-          {error.title ? (
-            <label className={style.labelError}>Invalid | Enter a name</label>
-          ) : null}
-        </div>
-        <input
-          required
-          id="title"
-          type="text"
-          name="title"
-          disabled={loading}
-          value={recipe.title}
-          placeholder="Enter the name"
-          autoFocus
-          onChange={handleValidation}
-          style={
-            error.title
-              ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
-              : {}
-          }
-        />
-
-        <div className={style.message}>
-          <label htmlFor="score" className={style.name}>
-            Score⭐
-          </label>
-          {error.score ? (
-            <label className={style.labelError}>
-              Invalid | Min: 1 - Max: 100
-            </label>
-          ) : null}
-        </div>
-        <input
-          required
-          min={1}
-          max={100}
-          id="score"
-          name="score"
-          type="number"
-          disabled={loading}
-          value={recipe.score}
-          onChange={handleValidation}
-          placeholder="Enter the score"
-          style={
-            error.score
-              ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
-              : {}
-          }
-        />
-
-        <div className={style.message}>
-          <label htmlFor="healthScore" className={style.name}>
-            HealthScore🌿
-          </label>
-          {error.healthScore ? (
-            <label className={style.labelError}>
-              Invalid | Min:1 - Max:100
-            </label>
-          ) : null}
-        </div>
-        <input
-          min={1}
-          max={100}
-          required
-          type="number"
-          id="healthScore"
-          name="healthScore"
-          disabled={loading}
-          value={recipe.healthScore}
-          placeholder="Enter the health score"
-          style={
-            error.healthScore
-              ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
-              : {}
-          }
-          onChange={handleValidation}
-        />
-
-        <div className={style.message}>
-          <label htmlFor="image" className={style.name}>
-            Image
-          </label>
-          {error.image ? (
-            <label className={style.labelError}>Invalid | Wrong URL</label>
-          ) : null}
-        </div>
-        <input
-          required
-          type="url"
-          id="image"
-          name="image"
-          disabled={loading}
-          value={recipe.image}
-          placeholder="Enter the image by url"
-          onChange={handleValidation}
-          style={
-            error.image
-              ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
-              : {}
-          }
-        />
-      </div>
-
-      <div className={style.image}>
-        <img src={recipe.image} alt="Imagen not found" />
-      </div>
-
-      <div className={style.large}>
-        <div className={style.message}>
-          <label htmlFor="summary" className={style.name}>
-            Summary
-          </label>
-          {error.summary ? (
-            <label className={style.labelError}>
-              Invalid | Enter a summary
-            </label>
-          ) : null}
-        </div>
-        <textarea
-          rows={5}
-          required
-          name="summary"
-          disabled={loading}
-          value={recipe.summary}
-          onChange={handleValidation}
-          placeholder="Enter the summary"
-          style={
-            error.summary
-              ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
-              : {}
-          }
-        />
-
-        <div className={style.message}>
-          <label htmlFor="steps" className={style.name}>
-            Steps
-          </label>
-          {error.steps ? (
-            <label className={style.labelError}>
-              Invalid | Enter the steps
-            </label>
-          ) : null}
-        </div>
-        <textarea
-          rows={5}
-          required
-          name="steps"
-          disabled={loading}
-          value={recipe.steps}
-          onChange={handleValidation}
-          placeholder="Enter the steps"
-          style={
-            error.steps
-              ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
-              : {}
-          }
-        />
-      </div>
-
-      <div className={style.diets}>
-        <div className={style.gridDiet}>
-          <label htmlFor="" className={style.name}>
-            Diets
-          </label>
-          {error.diets ? (
-            <label className={style.labelError}>
-              Invalid | Select at least one
-            </label>
-          ) : null}
-        </div>
-        {diets.length > 0 ? (
-          diets.map(diet => (
-            <div className={style.diet} key={diet.id}>
-              <input
-                name="diets"
-                type="checkbox"
-                value={diet.id}
-                disabled={loading}
-                checked={recipe.diets.includes(`${diet.id}`)}
-                onChange={handleValidation}
-              />
-              <label htmlFor="diets">{diet.name}</label>
-            </div>
-          ))
-        ) : (
-          <div>
-            <p>No hay dietas para seleccionar</p>
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>Name</label>
+            {error.title && (
+              <label className={style.labelError}>Invalid | Enter a name</label>
+            )}
           </div>
-        )}
-      </div>
-      {loading ? (
-        <div className={style.contentLoad}>
-          <Loading />
-        </div>
-      ) : (
-        <div className={style.buttons}>
           <input
-            type="submit"
-            value="Create"
-            className={activeSubmit ? style.error : style.submit}
-            disabled={activeSubmit}
+            required
+            autoFocus
+            id="title"
+            type="text"
+            name="title"
+            disabled={loading}
+            value={recipe.title}
+            placeholder="Enter the name"
+            onChange={handleValidation}
+            className={style.inputSection}
+            style={
+              error.title
+                ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
+                : {}
+            }
           />
-          <Link to="/home" className={style.link}>
-            Home
-          </Link>
         </div>
-      )}
+
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>Score ⭐</label>
+            {error.score && (
+              <label className={style.labelError}>
+                Invalid | Min: 1 - Max: 100
+              </label>
+            )}
+          </div>
+          <input
+            min={1}
+            required
+            max={100}
+            id="score"
+            name="score"
+            type="number"
+            disabled={loading}
+            value={recipe.score}
+            onChange={handleValidation}
+            placeholder="Enter the score"
+            className={style.inputSection}
+            style={
+              error.score
+                ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
+                : {}
+            }
+          />
+        </div>
+
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>HealthScore 🌿</label>
+            {error.healthScore && (
+              <label className={style.labelError}>
+                Invalid | Min:1 - Max:100
+              </label>
+            )}
+          </div>
+          <input
+            min={1}
+            max={100}
+            required
+            type="number"
+            id="healthScore"
+            name="healthScore"
+            disabled={loading}
+            value={recipe.healthScore}
+            onChange={handleValidation}
+            className={style.inputSection}
+            placeholder="Enter the health score"
+            style={
+              error.healthScore
+                ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
+                : {}
+            }
+          />
+        </div>
+
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>Image</label>
+            {error.image && (
+              <label className={style.labelError}>Invalid | Wrong URL</label>
+            )}
+          </div>
+          <input
+            required
+            type="url"
+            id="image"
+            name="image"
+            disabled={loading}
+            value={recipe.image}
+            onChange={handleValidation}
+            className={style.inputSection}
+            placeholder="Enter the image by url"
+            style={
+              error.image
+                ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
+                : {}
+            }
+          />
+        </div>
+
+        <div className={style.sectionsForm}>
+          <img src={recipe.image} alt="Imagen not found" />
+        </div>
+
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>Summary</label>
+            {error.summary && (
+              <label className={style.labelError}>
+                Invalid | Enter a summary
+              </label>
+            )}
+          </div>
+          <textarea
+            rows={5}
+            required
+            name="summary"
+            disabled={loading}
+            value={recipe.summary}
+            onChange={handleValidation}
+            className={style.inputSection}
+            placeholder="Enter the summary"
+            style={
+              error.summary
+                ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
+                : {}
+            }
+          />
+        </div>
+
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>Steps</label>
+            {error.steps && (
+              <label className={style.labelError}>
+                Invalid | Enter the steps
+              </label>
+            )}
+          </div>
+          <textarea
+            rows={5}
+            required
+            name="steps"
+            disabled={loading}
+            value={recipe.steps}
+            onChange={handleValidation}
+            placeholder="Enter the steps"
+            className={style.inputSection}
+            style={
+              error.steps
+                ? { borderBottomColor: "#E8FF06", backgroundColor: "#E8FF0638" }
+                : {}
+            }
+          />
+        </div>
+
+        <div className={style.sectionsForm}>
+          <div className={style.contentTitleSection}>
+            <label className={style.titleSection}>Diets</label>
+            {error.diets && (
+              <label className={style.labelError}>
+                Invalid | Select at least one
+              </label>
+            )}
+          </div>
+          {diets.length > 0 ? (
+            diets.map(diet => (
+              <div className={style.diet} key={diet.id}>
+                <input
+                  name="diets"
+                  type="checkbox"
+                  value={diet.id}
+                  disabled={loading}
+                  onChange={handleValidation}
+                  checked={recipe.diets.includes(`${diet.id}`)}
+                />
+                <label htmlFor="diets">{diet.name}</label>
+              </div>
+            ))
+          ) : (
+            <div>
+              <p>No hay dietas para seleccionar</p>
+            </div>
+          )}
+        </div>
+
+        <div className={style.sectionsForm}>
+          {loading ? (
+            <div className={style.contentLoad}>
+              <Loading />
+            </div>
+          ) : (
+            <div className={style.buttons}>
+              <input
+                type="submit"
+                value="Create"
+                className={activeSubmit ? style.error : style.submit}
+                disabled={activeSubmit}
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </form>
   );
 }
